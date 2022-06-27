a8173147a22048ba993cf5a5d2fd629e8f349b07
Yann-ship-it <yy.iris85@gmail.com>
2022-06-27 14:02:55 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
-    <UsersMid />
+    <HeaderNav />
+
+    <div v-if="$route.path == '/'">
+      <!-- Si la route est / (Racine du site) -->
+      <h1 class="pt-5 font-weight-light">
+        Vos films préférés sont sur VueJS Moviee !
+      </h1>
+      <MoviesList :movies="movies" :loading="loading" :errored="errored" />
+    </div>
+
+    <div v-else>
+      <router-view :key="$route.fullPash"></router-view>
+    </div>
+
+    <FooterApp />
   </div>
 </template>
 
 <script>
-import UsersMid from './components/Users.vue'
 
-export default {
-  name: 'app',
-  components: {
-    UsersMid }
-}
 </script>
 
 <style>
---
+++ src/components/FooterApp.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="FooterApp">
+    
+  </div>
+</template>
+
+<script>
+// Début du script
+import axios from "axios"; //
+export default {
+  name: "FooterApp", // Le nom doit être en 2 mots obligatoirement
+  data() {
+    return {
+      users: null, // return users null, on met le tableau en null donc vide
+    };
+  },
+  created: function () {
+    // creted function, ceci execute le code à son lancement
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+      this.users = res.data;
+    });
+  },
+};
+</script> 
